88b8e3dfa44d50775fd12b9481b1db35a4713064
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-06-22 11:17:47 -0400

    Initial commit

---
+++ backend/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -0,0 +1,70 @@
+package org.yearup.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.yearup.models.Category;
+import org.yearup.models.Product;
+import org.yearup.service.CategoryService;
+import org.yearup.service.ProductService;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
+public class CategoriesController {
+    private final CategoryService categoryService;
+    private ProductService productService;
+
+    @Autowired
+    public CategoriesController(CategoryService categoryService, ProductService productService) {
+        this.categoryService = categoryService;
+        this.productService = productService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Category> getAll() {
+        return categoryService.getAllCategories();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> getById(@PathVariable int id) {
+        return categoryService.getById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // the url to return all products in category 1 would look like this
+    // https://localhost:8080/categories/1/products
+    @GetMapping("/{categoryId}/products")
+    public List<Product> getProductsById(@PathVariable int categoryId) {
+        // get a list of product by categoryId
+        return null;
+    }
+
+    // add annotation to call this method for a POST action
+    // add annotation to ensure that only an ADMIN can call this function
+    public ResponseEntity<Category> addCategory(@RequestBody Category category) {
+        // insert the category and return it with status 201 Created
+        return null;
+    }
+
+    // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public Category updateCategory(@PathVariable int id, @RequestBody Category category) {
+        // update the category by id and return the updated category (200 OK)
+        return null;
+    }
+
+
+    // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    // add annotation to ensure that only an ADMIN can call this function
+    public ResponseEntity<Void> deleteCategory(@PathVariable int id) {
+        // delete the category by id and return status 204 No Content
+        return null;
+    }
+}
---
+++ backend/src/main/java/org/yearup/service/CategoryService.java
@@ -0,0 +1,39 @@
+package org.yearup.service;
+
+import org.springframework.stereotype.Service;
+import org.yearup.models.Category;
+import org.yearup.repository.CategoryRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getById(int categoryId) {
+        return categoryRepository.findById(categoryId);
+    }
+
+    public Category create(Category category) {
+        // create a new category
+        return null;
+    }
+
+    public Category update(int categoryId, Category category) {
+        // update category and return the updated category
+        return null;
+    }
+
+    public void delete(int categoryId) {
+        // delete category
+    }
+}
