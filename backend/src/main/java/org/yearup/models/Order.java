80d9f899c81e08f2e88dc42777ca5fa76b97f517
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-06-24 13:31:14 -0400

    Add OrderLineItem entity

---
+++ backend/src/main/java/org/yearup/models/Order.java
@@ -0,0 +1,8 @@
+package org.yearup.models;
+
+
+
+
+public class Order {
+
+}
---
+++ backend/src/main/java/org/yearup/models/OrderLineItem.java
@@ -0,0 +1,87 @@
+package org.yearup.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_line_items")
+public class OrderLineItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "order_line_item_id")
+    private int orderLineItemId;
+
+    @Column(name = "order_id")
+    private int orderId;
+
+    @Column(name = "product_id")
+    private int productId;
+
+    @Column(name = "sales_price")
+    private double price;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    @Column(name = "discount")
+    private double discount;
+
+    public OrderLineItem() {
+    }
+
+    public OrderLineItem(int orderLineItemId, int orderId, int productId, double price, int quantity, double discount) {
+        this.orderLineItemId = orderLineItemId;
+        this.orderId = orderId;
+        this.productId = productId;
+        this.price = price;
+        this.quantity = quantity;
+        this.discount = discount;
+    }
+
+    public int getOrderLineItemId() {
+        return orderLineItemId;
+    }
+
+    public void setOrderLineItemId(int orderLineItemId) {
+        this.orderLineItemId = orderLineItemId;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+}
