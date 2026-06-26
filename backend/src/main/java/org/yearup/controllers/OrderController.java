8e4018ab70d8a0a53c7a52ddc6823a32ddfa3c44
thetimstarcev <132023910+thetimstarcev@users.noreply.github.com>
2026-06-25 22:44:43 -0400

    Implement order checkout controller

--- backend/src/main/java/org/yearup/controllers/OrderController.java
+++ backend/src/main/java/org/yearup/controllers/OrderController.java
@@ -1,24 +1,40 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.yearup.models.Order;
+import org.yearup.models.User;
 import org.yearup.service.OrderService;
 import org.yearup.service.UserService;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/cart")
+@RequestMapping("/orders")
 @PreAuthorize("hasRole('USER')")
+@CrossOrigin
 public class OrderController {
-    private OrderService orderService;
-    private UserService userService;
+    private final OrderService orderService;
+    private final UserService userService;
 
     @Autowired
     public OrderController(OrderService orderService, UserService userService) {
         this.orderService = orderService;
         this.userService = userService;
     }
-    
+
+    @PostMapping
+    public ResponseEntity<Order> addOrder(Principal principal) {
+        String userName = principal.getName();
+        User user = userService.getByUserName(userName);
+        int userId = user.getId();
+        Order order = orderService.checkout(userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(order);
+    }
 }
